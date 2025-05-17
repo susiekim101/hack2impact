c17e9d2b16c4eca4f77bb992267547a73beed0db
jessicaodno <jessicaodonchimeg5@gmail.com>
2025-05-17 03:20:32 -0700

    hiii

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../Home.css'; // Assuming you have a CSS file for styling
+
 
 function Home() {
+    const navigate = useNavigate();
+
+    const handleStart = () => {
+        navigate('/quiz');
+    };
     return (
         <>
-            <h1>Hello</h1>
+            <div className="heading-container">
+                <h4 className="heading-small">Interior Design Quiz</h4>
+                <h1 className="heading-large">TORI IN THE HOUSE</h1>
+            </div>
+            
+            <div className="button-container">
+                <button className="rounded-button" onClick={handleStart}> START </button>
+            </div>
         </>
     );
 }
