b7391059750cd16215ae3bcdc7a30f0a1570794a
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-05-17 09:40:51 -0700

    css modules, redesign landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import logo from '../assets/torilogo.png';
+import styles from '../css/Home.module.css';
 
 function Home() {
     return (
-        <>
-            <h1>Hello</h1>
-        </>
+        <div className={styles.container}>
+            <img src={styles.logo} alt="Tori in the House Logo" className="logo-image"/>
+            <h1 class='text-sm'>Interior Design Quiz</h1>
+        </div>
     );
 }
 
