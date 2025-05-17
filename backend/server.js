1ed450eaefe77248306a7185a66ab38b1784355c
Lavender Hwang <lhwang914@gmail.com>
2025-05-17 16:25:44 -0700

    Fix Gemini text generation

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import textRoutes from './routes/textGeneration.js';
-import imageRoutes from './routes/imageGeneration.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import textRoutes from "./routes/textGeneration.js";
+import imageRoutes from "./routes/imageGeneration.js";
 
 dotenv.config();
 
-
 const app = express();
-const port = 5000;
+const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', textRoutes);
-app.use('/api', imageRoute);
+app.use("/api", textRoutes);
+app.use("/api", imageRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
 
 /*
 const API_KEY = process.env.GEMINI_API_KEY;
