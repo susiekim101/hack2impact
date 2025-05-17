5a87afb859adc6c6b8007cad27e819b010b6e5a6
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-05-17 00:36:10 -0700

    update quiz ui

--- src/components/quizBar.jsx
+++ src/components/quizBar.jsx
@@ -1,7 +1,20 @@
-const QuizBar = (currentQuestion) => {
+import '../css/QuizBar.css';
 
+const QuizBar = ({currentQuestion}) => {
+    const totalQuestions = 15;
+    const circles = [];
+
+    for(let i = 0; i < totalQuestions; i++) {
+        const isActive = i < currentQuestion;
+        circles.push(
+            <div key={i} className={ isActive ? "completed" : "remaining"}/>
+        )
+    }
     return (
         <>
+        <div className="row">
+            {circles}
+        </div>
         </>
 
     );
