140426e3547b994f80f4cf54a08a4faefc645b88
Lavender Hwang <lhwang914@gmail.com>
2025-05-17 16:30:12 -0700

    Fix merge conflicts

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,6 @@ app.use(express.json());
 
 app.use("/api", textRoutes);
 app.use("/api", imageRoutes);
-app.use("/api", textRoutes);
-app.use("/api", imageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
