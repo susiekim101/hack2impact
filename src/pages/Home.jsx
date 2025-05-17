0a89e29fb5d568edeffc7b204539d1c261883ece
Lavender Hwang <lhwang914@gmail.com>
2025-05-17 10:58:40 -0700

    fix routing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,41 @@
-import logo from '../assets/torilogo.png';
-import styles from '../css/Home.module.css';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../Home.css'; // Assuming you have a CSS file for styling
+import logo from "../assets/torilogo.png";
+import styles from "../css/Home.module.css";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../Home.css";
 
 function Home() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClick = () => {
-        navigate('/quiz');
-    };
+  const navigateQuiz = () => {
+    navigate("/quiz");
+  };
 
-    return (
-        <div className={styles.parentContainer}>
-            <div className={styles.titleContainer}>
-                <img src={logo} alt="Tori in the House Logo" className={styles.logoImage}/>
-                <h1 class='text-sm'>Interior Design Quiz</h1>
-            </div>
+  const navigateLogin = () => {
+    navigate("/login");
+  };
 
-            <div className={styles.buttonContainer}>
-                <button onClick={handleClick} className={styles.button}>Take Quiz</button>
-                <button onClick={handleClick} className={styles.button}>Log In</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.parentContainer}>
+      <div className={styles.titleContainer}>
+        <img
+          src={logo}
+          alt="Tori in the House Logo"
+          className={styles.logoImage}
+        />
+        <h1 class="text-sm">Interior Design Quiz</h1>
+      </div>
+
+      <div className={styles.buttonContainer}>
+        <button onClick={navigateQuiz} className={styles.button}>
+          Take Quiz
+        </button>
+        <button onClick={navigateLogin} className={styles.button}>
+          Log In
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
