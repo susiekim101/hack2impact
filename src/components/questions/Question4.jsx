bd0ece614377a4478399d2ed5f0cb28a6ae9b78e
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-05-17 21:27:05 -0700

    implement question 2-4

---
+++ src/components/questions/Question4.jsx
@@ -0,0 +1,33 @@
+import quizQuestions from "../../utils/quizQuestions";
+import styles from "../../css/questions/Question4.module.css";
+
+const Question4 = ({ formValues, setFormValues }) => {
+    const questionId = "preferredScent"
+    const options = quizQuestions[1].questions[2].options;
+
+    const selected = formValues[questionId];
+    const handleSelect = (optionValue) => {
+        setFormValues( (prev) => ({
+            ...prev,
+            [questionId]: optionValue,
+        }));
+    };
+
+    return (
+        <div className={styles.answerContainer}>
+            <div className={styles.multipleChoice}>
+                {options.map( (opt, idx) => (
+                    <div
+                        key={idx}
+                        className={`${styles.option} ${selected === opt ? styles.selected : ""}`}
+                        onClick={() => handleSelect(opt)}
+                    >
+                        {opt}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Question4;
